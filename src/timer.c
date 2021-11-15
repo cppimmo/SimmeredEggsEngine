0fc3c986be1524006dc7028c11a68222168bfe78
Brian Hoffpauir <brian@inspiron.bhoffpauir.com>
2021-11-14 23:13:27 -0600

    add timer src files

--- src/timer.c
+++ src/timer.c
@@ -1,3 +1,41 @@
 #include "timer.h"
+#include "SDL2/SDL.h"
 
+inline void timer_init(Timer *p_timer)
+{
+    p_timer->elapsed = 0;
+    p_timer->start = 0;
+    p_timer->end = 0;
+    p_timer->paused = true;
+}
 
+inline void timer_start(Timer *p_timer)
+{
+    if (p_timer->paused)
+	return;
+    p_timer->start = SDL_GetTicks();
+}
+    
+inline void timer_stop(Timer *p_timer)
+{
+    if (p_timer->paused)
+	return;
+    p_timer->end = SDL_GetTicks();
+}
+    
+inline bool timer_paused(Timer *p_timer)
+{
+    p_timer->paused = !p_timer->paused;
+}
+
+inline void timer_update(Timer *p_timer)
+{
+    if (p_timer->paused)
+	return;
+    p_timer->elapsed = p_timer->end - p_timer->start;
+}
+
+inline uint64_t timer_peek_ms(Timer *p_timer)
+{
+    return p_timer->elapsed;
+}
--- src/timer.h
+++ src/timer.h
@@ -1,6 +1,23 @@
 #ifndef TIMER_H
 #define TIMER_H
 
+#include <stdlib.h>
+#include <stdbool.h>
+#include <stdint.h>
 
+struct timer_t {
+    uint64_t elapsed;
+    uint64_t start;
+    uint64_t end;
+    bool paused;
+};
+typedef struct timer_t Timer;
+
+void timer_init(Timer *p_timer);
+void timer_start(Timer *p_timer);
+void timer_stop(Timer *p_timer);
+bool timer_paused(Timer *p_timer);
+void timer_update(Timer *p_timer);
+uint64_t timer_peek_ms(Timer *p_timer);
 
 #endif // TIMER_H
