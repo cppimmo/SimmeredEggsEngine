db311857da38c826fb13ec65fc5c983f118b3c8a
Brian Hoffpauir <bhoffpauir0424@gmail.com>
2021-11-21 23:48:20 -0600

    update config; not yet loaded

--- src/config.c
+++ src/config.c
@@ -1,18 +1,32 @@
 #include "config.h"
+#include <unistd.h>
 
-bool config_load(const char *filename, Options *pOtions)
+static FILE *p_config_file = NULL;
+
+bool config_load(const char *filename, Options *p_options)
 {
-	if (!(pConfigFile = fopen(CONFIG_FILE, "r"))) {
+	p_config_file = fopen(filename, "r");
+	if (p_config_file == NULL) {
 		fprintf(stderr, "Failed to reading configuration file.\n");
 		return false;
 	}
+	
+	char *line = NULL;
+	size_t len = 0;
+	ssize_t nread;
+	while ((nread = getline(&line, &len, p_config_file)) != -1) {
+		printf("Retrieved line of length %zd:\n", nread);
+		fwrite(line, nread, 1, stdout);
+	}
+
+	free(line);
 	return true;
 }
 
 void config_close()
 {
-	if (pConfigFile != NULL) {
-		fclose(pConfigFile);
+	if (p_config_file != NULL) {
+		fclose(p_config_file);
 	}
 }
 
--- src/config.h
+++ src/config.h
@@ -5,19 +5,19 @@
 #include <stdlib.h>
 #include <stdbool.h>
 
-#define CONFIG_FILE "starshipfleet.conf"
-static FILE *pConfigFile = NULL;
+#define CONFIG_FILE "starshipfleet.ini"
 
 typedef struct {
 	char window_title[75];
 	size_t window_size_x;
 	size_t window_size_y;
+	bool is_fullscreen;
 	unsigned int refresh_rate;
 	bool vsync_enabled;
 	unsigned int graphics_quality;
 } Options;
 
-bool config_load(const char *filename, Options *pOptions);
+bool config_load(const char *filename, Options *p_options);
 void config_close();
 
 #endif // CONFIG_H
