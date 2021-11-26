349f13ebd137d0efdb1a2799efc6f5fcc962420b
Brian Hoffpauir <bhoffpauir0424@gmail.com>
2021-11-25 23:42:55 -0600

    add drawing function signatures to render.h

--- src/render.h
+++ src/render.h
@@ -5,6 +5,9 @@
 #include "GL/glew.h"
 #include <stdbool.h>
 
+#include "vaobject.h"
+#include "vbuffer.h"
+
 // offset, and type
 #define BUFFER_OFFSET(X, Y) ((void*)(X* sizeof(Y)))
 
@@ -14,6 +17,9 @@ GLboolean r_is_enabled(GLenum capability);
 void r_enable(GLenum capability);
 void r_disable(GLenum capability);
 void r_wireframe(const bool value);
+void r_draw(GLenum mode, GLint first, GLsizei count);
+void r_draw_indexed(GLenum mode, GLsizei count, GLenum type,
+					const GLvoid *indices);
 void r_message_callback(GLDEBUGPROC callback, void *userParam);
 void r_message_control(GLenum source, GLenum type, GLenum severity,
 					   GLsizei count, const GLuint *ids, GLboolean enabled);
