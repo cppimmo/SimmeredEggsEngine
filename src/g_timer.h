b13aadb0d69e5f51a64f9a4039fb1fa061d38cde
Brian Hoffpauir <bhoffpauir0424@gmail.com>
2021-12-06 23:36:23 -0600

    add LICENSE header to all src files

--- src/g_timer.h
+++ src/g_timer.h
@@ -1,3 +1,27 @@
+/* =============================================================================
+** SimmeredEggsEngine, file: g_timer.h Created 12/6/2021
+**
+** Copyright 2021 Brian Hoffpauir TX, USA
+** All rights reserved.
+**
+** Redistribution and use of this source file, with or without modification, is
+** permitted provided that the following conditions are met:
+**
+** 1. Redistributions of this source file must retain the above copyright
+**    notice, this list of conditions and the following disclaimer.
+**
+** THIS SOFTWARE IS PROVIDED BY THE AUTHOR "AS IS" AND ANY EXPRESS OR IMPLIED
+** WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+** MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.  IN NO
+** EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+** SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+** PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+** OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+** WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+** OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+** ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+** =============================================================================
+**/
 #ifndef __G_TIMER_H__
 #define __G_TIMER_H__
 
